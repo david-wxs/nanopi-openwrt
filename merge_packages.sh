752edd23e31a145d063acee233d2ff85d0adc031
John Doe <klever1988@protonmail.com>
2021-03-07 12:00:13 +0800

    script: fix package merging

--- merge_packages.sh
+++ merge_packages.sh
@@ -1,10 +1,9 @@
-git clone --depth=1 https://github.com/project-lede/luci-app-godproxy && rm -r package/luci-app-godproxy && mv luci-app-godproxy package/
-git clone --depth=1 https://github.com/destan19/OpenAppFilter.git && rm -r package/OpenAppFilter && mv OpenAppFilter package/
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git && rm -r package/lean/luci-theme-argon && mv luci-theme-argon package/lean/
+git clone --depth=1 https://github.com/project-lede/luci-app-godproxy && merge_packge luci-app-godproxy
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git && merge_packge luci-theme-argon
 
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifsd && rm -r package/lean/luci-app-ksmbd && mv luci-app-cifsd package/lean/
-svn co https://github.com/coolsnowwolf/packages/trunk/kernel/ksmbd && rm -r package/feeds/packages/ksmbd && mv ksmbd package/feeds/packages/
-svn co https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools && rm -r package/feeds/packages/ksmbd-tools && mv ksmbd-tools package/feeds/packages/
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifsd && merge_packge luci-app-ksmbd
+svn co https://github.com/coolsnowwolf/packages/trunk/kernel/ksmbd && merge_packge ksmbd
+svn co https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools && merge_packge ksmbd-tools
 
 if [ $DEVICE = 'r2s' ]; then
 mkdir -p target/linux/rockchip/armv8/base-files/usr/bin &&\
@@ -16,7 +15,13 @@ chmod +x target/linux/rockchip/armv8/base-files/etc/init.d/fa-rk3328-pwmfan
 mkdir -p target/linux/rockchip/armv8/base-files/etc/rc.d &&\
 ln -sf ../init.d/fa-rk3328-pwmfan target/linux/rockchip/armv8/base-files/etc/rc.d/S96fa-rk3328-pwmfan
 
-git clone --depth=1 https://github.com/NateLol/luci-app-oled && rm -r package/luci-app-oled && mv luci-app-oled package/
+git clone --depth=1 https://github.com/NateLol/luci-app-oled && merge_packge luci-app-oled
 fi
 
 sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
+
+function merge_packge(){
+    rm -rf $1/.git
+    find package/ -type d -name $1 | xargs -r rm -r
+    mv $1 package/
+}
